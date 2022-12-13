22d30141aa60fa041de01b71f4fc1f348d81cd10
ShunMiura <shun13andcooki3.1999@gmail.com>
2022-12-13 17:33:43 +0900

    三浦)3DのCubeから2Dのオブジェクトに変更したら床をすり抜けるようになりました。

--- Assets/Script/Miura/Enemy.cs
+++ Assets/Script/Miura/Enemy.cs
@@ -9,34 +9,39 @@ public class Enemy : MonoBehaviour
 {
     // グローバル変数みたいにフィールドを使います
     public int State = 1; // エネミーの状態を入れる変数。初期値は敵として襲いかかってくる状態
-    public float MoveTime = 1.5f;  // 行動が変わる時間
+    public float MoveTime = 10f;  // 行動が変わる時間
     float time = 0f;
-
+    float movex = 0.001f;   // 移動量
+                            // 現在のポジション取得
+    Vector3 MoveValue;
     void Start()
     {
         time = 0f;
+        State = 1;
     }
 
     void Update()
     {
-        // 現在のポジション取得
-        Vector2 pos = transform.position;
-        float movex = 0.001f;
 
-        // 使ってみたはいいが、、、
+
+
+        // 使ってみたはいいが、使い方がわからない！！
         time += Time.deltaTime;
 
         if (State == 1)// 1:敵として襲いかかってくる状態
         {
-            // 襲いかかってくる動き
-            MoveTime -= 1;
-            if(MoveTime < time)
+            // 襲いかかってくる動き,一定時間経過でPOS動かしたい。
+            if (MoveTime < time)
             {
                 time = 0f;
                 movex *= -1;
             }
 
-            pos.x -= movex;
+
+
+            // この辺に地面と一定の距離検知したら、上に力を加える処理書いたら、落ちなそう
+
+
 
             // Playerの当たり判定を検知したら、状態(State)を2(持ち運ばれ状態)に変更
             //if ()
@@ -63,8 +68,10 @@ public class Enemy : MonoBehaviour
 
         }
 
+        // 移動を反映する前にふわふわした慣性をつけるといいのかな？？
+
         // 移動を反映
-        transform.position = pos;
+        transform.Translate(MoveValue);
 
         // 画面外判定どうする？？
 
