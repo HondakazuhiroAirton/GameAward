e20d3f5cf1ce37da8b4e61b0e3f6800abf64cbfc
motaikouta <mrkaito1504@gmail.com>
2022-12-14 19:48:22 +0900

    甕)雲、カミナリ、mobのオブジェクトを作り、↓キーを押すと雲からカミナリが出てくるプログラムを作りました。

---
+++ Assets/Script/motaiScript/cloud.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloud : MonoBehaviour
+{
+    public GameObject thunder;
+    Vector3 createPosition;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            createPosition = transform.position;
+            createPosition.y -= 1.0f; // â_ÇÃyç¿ïW - 1ÇÃà íuÇ…óãÇê∂ê¨Ç∑ÇÈ
+
+            Instantiate(thunder, createPosition, Quaternion.identity);
+        }
+
+    }
+}
