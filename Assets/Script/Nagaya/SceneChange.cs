12f10966bde62ffab394490810fb36f6b114d702
mkngy <halkirby0423@gmail.com>
2022-12-14 13:54:26 +0900

    長屋）Spaceキーを押すとTitleへ、Enterキーを押すとEndシーンへ飛ぶスクリプトを作りました。それにともなって自分用のシーンファイルを作っています

--- Assets/Script/Nagaya/SceneChange.cs
+++ Assets/Script/Nagaya/SceneChange.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;  //SceneManagementの機能を使用
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //もし入力されたキーがspaceキーなら中の処理を実行する
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //TitleSceneに切り替え
+            SceneManager.LoadScene("TitleScene(Nagaya)");
+        }
+
+        //もし入力されたキーがWキーなら
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //TitleSceneに切り替え
+            SceneManager.LoadScene("NagayaScene");
+        }
+
+        //もし入力されたキーがEnterKeyなら
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //EndSceneに切り替え
+            SceneManager.LoadScene("EndScene");
+        }
     }
+
+    
 }
+
