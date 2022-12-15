54f5fd659d5a7ddb6b4272c65cc925d34b6e3acb
mkngy <halkirby0423@gmail.com>
2022-12-15 17:15:14 +0900

    長屋）丸の物体が四角に当たるとEndSceneに行くプログラムを作りました！！！

--- Assets/Script/Nagaya/CircleCollision.cs
+++ Assets/Script/Nagaya/CircleCollision.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CageCollision : MonoBehaviour
+public class CircleCollision : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene("EndScene");
     }
 }
