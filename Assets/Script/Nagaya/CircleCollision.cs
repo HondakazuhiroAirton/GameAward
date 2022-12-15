7742b01a68156c8e08128cdc349c71aaa501978b
mkngy <halkirby0423@gmail.com>
2022-12-15 19:49:22 +0900

    長屋）sourcetreeにてエラーが発生していたので再度コミットします。内容は一緒です。

--- Assets/Script/Nagaya/CircleCollision.cs
+++ Assets/Script/Nagaya/CircleCollision.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CageCollision : MonoBehaviour
+public class CircleCollision : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
@@ -14,4 +16,11 @@ public class CageCollision : MonoBehaviour
     {
         
     }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+
 }
