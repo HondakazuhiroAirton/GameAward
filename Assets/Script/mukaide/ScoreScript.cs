6d6ca608484f85133d37fa99f1f4e2c51b126b9c
syu_mukaide <mukaide.syu@gmail.com>
2022-12-20 22:59:50 +0900

    向出)スコアの保存

--- Assets/Script/mukaide/ScoreScript.cs
+++ Assets/Script/mukaide/ScoreScript.cs
@@ -5,13 +5,22 @@ using TMPro;
 
 public class ScoreScript : MonoBehaviour
 {
-    int Score = 0;
     public TextMeshProUGUI ScoreText;
+    int Score = 0;
+    int highScore = 0; // ハイスコアを保持
 
     // Start is called before the first frame update
     void Start()
     {
+        Score = PlayerPrefs.GetInt("Score",0);
         this.ScoreText.text = "Score" + 0.ToString();
+    }
+
+    void OnDestroy()
+    {
+        // スコアを保存
+        PlayerPrefs.SetInt("Score", Score);
+        PlayerPrefs.Save();
     }
 
     // Update is called once per frame
@@ -23,6 +32,13 @@ public class ScoreScript : MonoBehaviour
         {
             GameObject.Find("Canvas").GetComponent<ScoreScript>().AddScore();
         }
+
+        //スコア保存確認用
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Score = 0;
+        }
+
     }
 
     public void AddScore()
@@ -30,4 +46,6 @@ public class ScoreScript : MonoBehaviour
         this.Score += 10;
         this.ScoreText.text = "Score" + Score.ToString();
     }
+
+  
 }
