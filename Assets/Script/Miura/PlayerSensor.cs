424d7ae69b54539b061d352f9a22a7d70253d73c
ShunMiura <shun13andcooki3.1999@gmail.com>
2023-01-21 12:52:03 +0900

    三浦）できたようなできてないような、微妙です。 テクスチャを貼ってみましたが巨大になってしまいました。

--- Assets/Script/Miura/PlayerSensor.cs
+++ Assets/Script/Miura/PlayerSensor.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerSensor : MonoBehaviour
 {
+
+    Vector2 EnemyPos;
+    Vector2 CollisionEnemyPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,48 @@ public class PlayerSensor : MonoBehaviour
         {
             Debug.Log("ŽË’öŒ—“à‚¾‚æ");
             transform.parent.GetComponent<Enemy>().SetEnemyState(2);
+        }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("“G‚ð”ð‚¯‚é‚æ");
+            EnemyPos = transform.parent.GetComponent<Enemy>().transform.position;
+            CollisionEnemyPos = collision.transform.position;
+            Vector2 Compare = new Vector2(EnemyPos.x - CollisionEnemyPos.x, EnemyPos.y - CollisionEnemyPos.y);
+
+            if (Compare.x >= 0)
+            {
+                if (Compare.y >= 0)
+                {
+                    EnemyPos.x = EnemyPos.x + 100;
+                    EnemyPos.y = EnemyPos.y + 100;
+                    this.transform.position = EnemyPos;
+                    //transform.parent.GetComponent<Enemy>().rb.AddForce(new Vector2(1.5f, 1f));
+                }
+                else
+                {
+                    EnemyPos.x = EnemyPos.x + 100;
+                    EnemyPos.y = EnemyPos.y - 100;
+                    this.transform.position = EnemyPos;
+                    //transform.parent.GetComponent<Enemy>().rb.AddForce(new Vector2(1.5f, -1f));
+                }
+            }
+            else if (Compare.x < 0)
+            {
+                if (Compare.y >= 0)
+                {
+                    EnemyPos.x = EnemyPos.x - 100;
+                    EnemyPos.y = EnemyPos.y + 100;
+                    this.transform.position = EnemyPos;
+                    //transform.parent.GetComponent<Enemy>().rb.AddForce(new Vector2(-1.5f,1f));
+                }
+                else
+                {
+                    EnemyPos.x = EnemyPos.x - 100;
+                    EnemyPos.y = EnemyPos.y - 100;
+                    this.transform.position = EnemyPos;
+                   // transform.parent.GetComponent<Enemy>().rb.AddForce(new Vector2(-1.5f, -1f));
+                }
+            }
         }
     }
 
