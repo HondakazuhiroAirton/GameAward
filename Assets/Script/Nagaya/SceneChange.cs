986a75774a7e3783b5c3086277f0dd2eddc88632
mkngy <halkirby0423@gmail.com>
2023-01-23 12:32:20 +0900

    タイトル画面からゲームプレイ画面のシーン遷移をやりました！

--- Assets/Script/Nagaya/SceneChange.cs
+++ Assets/Script/Nagaya/SceneChange.cs
@@ -11,25 +11,25 @@ public class SceneChange : MonoBehaviour
     void Update()
     {
         //もし入力されたキーがspaceキーなら中の処理を実行する
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //TitleSceneに切り替え
-            SceneManager.LoadScene("TitleScene(Nagaya)");
-        }
+       // if (Input.GetKeyDown(KeyCode.Space))
+       // {
+       //     //TitleSceneに切り替え
+       //     SceneManager.LoadScene("TitleScene(Nagaya)");
+       // }
 
-        //もし入力されたキーがWキーなら
-        if (Input.GetKeyDown(KeyCode.W))
+        //もし入力されたキーがEnterキーなら
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             //TitleSceneに切り替え
             SceneManager.LoadScene("NagayaScene");
         }
 
         //もし入力されたキーがEnterKeyなら
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            //EndSceneに切り替え
-            SceneManager.LoadScene("EndScene");
-        }
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    //EndSceneに切り替え
+        //    SceneManager.LoadScene("GameOverScene");
+        //}
     }
 
     
