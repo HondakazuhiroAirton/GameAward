cc0f78f293aada76520e4fb5dc04e5b984eabb6a
mkngy <halkirby0423@gmail.com>
2023-01-27 14:17:47 +0900

    長屋）シーン遷移を一通り入れましたが、まだまだエラーが出ています

--- Assets/Script/Nagaya/CircleCollision.cs
+++ Assets/Script/Nagaya/CircleCollision.cs
@@ -8,20 +8,38 @@ public class CircleCollision : MonoBehaviour
 {
     public TextMeshProUGUI text;
     public int addText = 1;
-    
+    GameObject MainScene;
+    GameObject SecondStage;
+    GameObject ThirdStage;
+    GameObject EndScene;
+
+    public static List<string> GetActiceSceneNames()
+    {
+        int sceneCount = SceneManager.sceneCount;
+        List<string> activeSceneNames = new List<string>();
+        for (int i = 0; i < sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+            activeSceneNames.Add(scene.name);
+        }
+        return activeSceneNames;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
 
         addText = 0;
+
+        Debug.Log(SceneManager.GetActiveScene().name);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //変数addTextを文字列に変換し、text.textに代入＝表示する文章を変更
-       text.text = addText.ToString();
+        text.text = addText.ToString();
     }
 
 
@@ -34,17 +52,41 @@ public class CircleCollision : MonoBehaviour
             Invoke("GoToGameOver", 1.0f);
         }
 
+    
+
+
+           int sceneCount = SceneManager.sceneCount;
+    
         if(other.gameObject.CompareTag("Triangle"))
-        {//
-            addText = addText + 1;
-            if (addText == 5)
+        {
+            addText+=1;
+
+            switch (sceneCount)
             {
-                SceneManager.LoadScene("SecondStage");   //５回当たったらクリア
+                case 1: //MainScene
+                    if (addText >= 5 && SceneManager.GetActiveScene().name == "MainScene")
+                    {
+                        SceneManager.LoadScene("SecondStage");
+                    }
+                    break;
+                case 2: //SecondStage
+                    if (addText >= 10 && SceneManager.GetActiveScene().name == "SecondStage")
+                    {
+                        SceneManager.LoadScene("ThirdStage");
+                    }
+                    break;
+                case 3: //ThirdStage
+                    if (addText >= 15 && SceneManager.GetActiveScene().name == "ThirdStage")
+                    {
+                        SceneManager.LoadScene("EndScene");
+                    }
+                    break;
             }
 
         }
-
-
+       
+    
+    
 
 
     }
@@ -54,9 +96,7 @@ public class CircleCollision : MonoBehaviour
         SceneManager.LoadScene("GameOverScene");
     }
 
-
-
-
-
+    
+    
 
 }
