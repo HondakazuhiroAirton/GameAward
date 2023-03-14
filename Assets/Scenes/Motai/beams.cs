573ab6ec253a80194c6748f21d20caf85a5b09ee
motaikouta <mrkaito1504@gmail.com>
2023-03-15 01:16:16 +0900

    甕)発射角度を変えたときその方向に発射できるようにした

--- Assets/Scenes/Motai/beams.cs
+++ Assets/Scenes/Motai/beams.cs
@@ -4,35 +4,28 @@ using UnityEngine;
 
 public class beams : MonoBehaviour
 {
-    //public ParticleSystem ps;
-    //GameObject obj;
-    //private Vector3 mousePosition;
-
     [SerializeField] GameObject particle;
     private Vector3 objPosition;
-    
-
+    private Quaternion objAng;
+    private Quaternion psAng;
+    public ParticleSystem ps;
 
     // Start is called before the first frame update
     void Start()
     {
-        //ps.Stop();
+        
     }
     
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetKey(KeyCode.A))
-        {
-            //mousePosition = Input.mousePosition;
-            //mousePosition.z = 3f;
-            //Instantiate(ps, Camera.main.ScreenToWorldPoint(mousePosition), Quaternion.identity);
-            //ps.Play();
-        }*/
         if (Input.GetKeyDown(KeyCode.A))
         {
             objPosition = this.gameObject.transform.position;
-            Instantiate(particle,objPosition, Quaternion.identity);   
+            objAng = this.gameObject.transform.rotation;
+            psAng = particle.transform.rotation;
+
+            Instantiate(particle,objPosition,objAng);   
         }
     }
 }
