7a1a52a25a0924a230327596a86914842c0d99f7
ShunMiura <shun13andcooki3.1999@gmail.com>
2023-03-16 14:16:19 +0900

    三浦）ビームの当たり判定作成中です。反射しなければいい感じになった。

--- Assets/Script/Miura/BeamCollision.cs
+++ Assets/Script/Miura/BeamCollision.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BeamCollision : MonoBehaviour
 {
+    // ビームパーティクルオブジェクト
+    public GameObject BeamParticle;
     // ビームが進む方向
     public Vector3 ParticlemoveDir;
     // ビームの回転角
@@ -11,41 +13,56 @@ public class BeamCollision : MonoBehaviour
     // ビームの移動スピード
     public float ParticleSpeed = 0.1f;
 
-    // 初期位置保存
+    // 当たり判定の初期位置保存
     protected Vector3 startPosition;
+    // パーティクルのポジション保存
+    protected Vector3 beamParticlePosition;
     // 回転補正値
-    protected float AngleHosei = 90; 
+    protected float AngleHosei = 45; 
 
     // Start is called before the first frame update
     void Start()
     {
         // ビームパーティクルからビームの進行方向をもらう
-        ParticlemoveDir = transform.parent.GetComponent<BeamParticleScript>().moveDir;
+        ParticlemoveDir = BeamParticle.GetComponent<BeamParticleScript>().moveDir;
 
         // ビームパーティクルからビームの回転方向ををもらう
-        ParticleAngle = transform.parent.GetComponent<BeamParticleScript>().Angle;
+        ParticleAngle = BeamParticle.GetComponent<BeamParticleScript>().Angle;
 
         // スピード値
         //ParticleSpeed = 0.1f;
 
-        // 初期位置もらってくる
+        // 初期位置取得
         startPosition = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.GetComponent<BoxCollider>().size += new Vector3 (0.0f,0.1f,0.0f);
+        // 少しずつ大きくなる
+        this.GetComponent<BoxCollider>().size += new Vector3 (0.0f, ParticleSpeed, 0.0f);
         
         // 補正値決定
         float xSize = this.GetComponent<BoxCollider>().size.x;
         float ySize = this.GetComponent<BoxCollider>().size.y;
         float zSize = this.GetComponent<BoxCollider>().size.z;
 
-        // BoxColliderサイズの半分分下に戻して補正
-        this.transform.position = startPosition + new Vector3 (xSize / 2,ySize / 2 ,zSize / 2);
+        // 1.BoxColliderの位置決定
+        // 毎フレームパーティクルの位置をもらう(この処理重いかも)
+        beamParticlePosition = BeamParticle.transform.position;
 
-        // 回転を反映
+        // 開始位置とビームの現在値を/2して移動量を出す
+        Vector3 collisionPosition;
+        collisionPosition = beamParticlePosition - startPosition;
+        collisionPosition = collisionPosition / 2;
+        Debug.Log("真ん中の値は" + collisionPosition + "ですにゃん。");
+
+        // スタートポジションに移動量を足してポジションを出す
+        collisionPosition = startPosition + collisionPosition;
+        // 位置を反映(おかしい)
+         this.transform.position = collisionPosition;
+
+        // 回転を反映(おかしい)
         this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, ParticleAngle - AngleHosei);
     }
 }
