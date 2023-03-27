867f4089127d0a2317b5fb462d81a09b876fd059
motaikouta <mrkaito1504@gmail.com>
2023-03-27 23:10:19 +0900

    甕)更新

--- Assets/Scenes/Motai/ParticleCollision.cs
+++ Assets/Scenes/Motai/ParticleCollision.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class ParticleCollision2 : MonoBehaviour
 {
-    //public Vector3 pos;
     private void OnParticleCollision(GameObject other)
     {
         if (other.gameObject.tag == "Enemy")
         {
             GameObject obj = GameObject.FindGameObjectWithTag("Enemy");
             Destroy(obj);
-        }
-        if (other.gameObject.tag == "Warp")
-        {
-            other.gameObject.transform.position = new Vector3(-50f,-97f,10);
         }
     }
 }
