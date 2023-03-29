293230672fccb565402248d1984ea202d824e814
mkngy <halkirby0423@gmail.com>
2023-03-29 23:04:16 +0900

    長屋）バックアップパート２

--- Assets/Script/Nagaya/ButtonChoice.cs
+++ Assets/Script/Nagaya/ButtonChoice.cs
@@ -5,18 +5,5 @@ using UnityEngine.UI;
 
 public class ButtonChoice : MonoBehaviour
 {
-    Button resume;
-    Button restart;
-    Button stageselect;
 
-    void Start()
-    {
-        resume = GameObject.Find("Canvas/Panel").GetComponent<Button>();
-        restart = GameObject.Find("Canvas/Panel").GetComponent<Button>();
-        stageselect = GameObject.Find("Canvas/Panel").GetComponent<Button>();
-
-
-        //ƒ{ƒ^ƒ“‚ª‘I‘ğ‚³‚ê‚½ó‘Ô‚É‚È‚é
-        resume.Select();
-    }
 }
