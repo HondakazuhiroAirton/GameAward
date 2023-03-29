af66e3589d57f74d3f3d5f6ff201484e2b516713
motaikouta <mrkaito1504@gmail.com>
2023-03-30 00:19:03 +0900

    甕)クラス作り始めました

---
+++ Assets/Script/Motai/class/PlayerClass.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClass : MonoBehaviour
+{
+    class Player
+    {
+        public int GetPlayerLife;// Žc‹@
+        public float GetBeamAmount;// Žc—Ê
+    }
+}
