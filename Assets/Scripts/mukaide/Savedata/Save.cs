b5dd6795c0767e1a2395f28c35291b38a30f1a8a
syu_mukaide <mukaide.syu@gmail.com>
2023-04-15 22:33:36 +0900

    向出）シーン

--- Assets/Scripts/mukaide/Savedata/Save.cs
+++ Assets/Scripts/mukaide/Savedata/Save.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Text;
 
 public class Save : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private StreamWriter sa;
+
     void Start()
     {
-        
+        sa = new StreamWriter(@"save.csv", true, Encoding.GetEncoding("Shift_JIS"));
+        string[] s1 = { "F", "J", "time" };
+        string s2 = string.Join(",", s1);
+        sa.WriteLine(s2);
     }
 
-    // Update is called once per frame
+    public void SaveData(string txt1)
+    {
+        string[] s1 = { txt1};
+        string s2 = string.Join(",", s1);
+        sa.WriteLine(s2);
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            sa.Close();
+        }
+
     }
 }
