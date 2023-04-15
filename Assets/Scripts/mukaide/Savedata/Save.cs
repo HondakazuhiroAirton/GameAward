2c54ed2a478cdca82c6957362f5679cf3399955f
syu_mukaide <mukaide.syu@gmail.com>
2023-04-16 01:06:21 +0900

    向出）csv書き込みできました

--- Assets/Scripts/mukaide/Savedata/Save.cs
+++ Assets/Scripts/mukaide/Savedata/Save.cs
@@ -6,29 +6,39 @@ using System.Text;
 
 public class Save : MonoBehaviour
 {
-    private StreamWriter sa;
+    private StreamWriter sw;
 
     void Start()
     {
-        sa = new StreamWriter(@"save.csv", true, Encoding.GetEncoding("Shift_JIS"));
-        string[] s1 = { "F", "J", "time" };
+        sw = new StreamWriter(@"SaveData.csv", true, Encoding.GetEncoding("Shift_JIS"));
+        string[] s1 = { "stage" };
         string s2 = string.Join(",", s1);
-        sa.WriteLine(s2);
+        sw.WriteLine(s2);
+
+        s2 = string.Join(",", "stage1");
+        sw.WriteLine(s2);
     }
 
     public void SaveData(string txt1)
     {
         string[] s1 = { txt1};
         string s2 = string.Join(",", s1);
-        sa.WriteLine(s2);
+        sw.WriteLine(s2);
     }
 
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+          SaveData("stage1");
+        }
+
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            sa.Close();
+            sw.Close();
         }
 
     }
 }
+
