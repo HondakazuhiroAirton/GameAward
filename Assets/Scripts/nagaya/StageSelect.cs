bf04c05518b7418755b88e6b6375f8d39e7b87ad
syu_mukaide <mukaide.syu@gmail.com>
2023-05-17 00:20:56 +0900

    向出）ボタンおしてもSEは一回しかなりません、たぶん

--- Assets/Scripts/nagaya/StageSelect.cs
+++ Assets/Scripts/nagaya/StageSelect.cs
@@ -20,12 +20,17 @@ public class StageSelect : MonoBehaviour
 
     Image fadeImage;                //透明度を変更するパネルのイメージ
 
+    //SE
+    public AudioManager SE;
+    private Button buttonflag;//ぼたんを一回だけ押す処理
 
     // Start is called before the first frame update
     void Start()
     {
         SM = GameObject.Find("StageManager");
         fadeImage = GameObject.Find("Image").GetComponent<Image>();
+        //SE
+        SE = GameObject.Find("AudioManage").GetComponent<AudioManager>();
     }
 
 
@@ -37,30 +42,45 @@ public class StageSelect : MonoBehaviour
 
     public void onButtonStage1_1()
     {
+        buttonflag = GameObject.Find("1-1").GetComponent<Button>();
+        buttonflag.interactable = false;
+        SE.playselectSE();
         fadeImage.GetComponent<Fade>().GameStart();
         SM.GetComponent<StageManagerScript>().SetStageIndex(0);
     }
 
     public void onButtonStage1_2()
     {
+        buttonflag = GameObject.Find("1-2").GetComponent<Button>();
+        buttonflag.interactable = false;
+        SE.playselectSE();
         fadeImage.GetComponent<Fade>().GameStart();
         SM.GetComponent<StageManagerScript>().SetStageIndex(1);
     }
 
     public void onButtonStage1_3()
     {
+        buttonflag = GameObject.Find("1-3").GetComponent<Button>();
+        buttonflag.interactable = false;
+        SE.playselectSE();
         fadeImage.GetComponent<Fade>().GameStart();
         SM.GetComponent<StageManagerScript>().SetStageIndex(2);
     }
 
     public void onButtonStage1_4()
     {
+        buttonflag = GameObject.Find("1-4").GetComponent<Button>();
+        buttonflag.interactable = false;
+        SE.playselectSE();
         fadeImage.GetComponent<Fade>().GameStart();
         SM.GetComponent<StageManagerScript>().SetStageIndex(3);
     }
 
     public void onButtonStage1_5()
     {
+        buttonflag = GameObject.Find("1-5").GetComponent<Button>();
+        buttonflag.interactable = false;
+        SE.playselectSE();
         fadeImage.GetComponent<Fade>().GameStart();
         SM.GetComponent<StageManagerScript>().SetStageIndex(4);
     }
--- Assets/Scripts/nagaya/StageSelect2.cs
+++ Assets/Scripts/nagaya/StageSelect2.cs
@@ -14,11 +14,18 @@ public class StageSelect2 : MonoBehaviour
 
     Image fadeImage;                //透明度を変更するパネルのイメージ
 
+    //SE
+    public AudioManager SE;
+    private Button buttonflag;//ぼたんを一回だけ押す処理
+
     // Start is called before the first frame update
     void Start()
     {
         SM = GameObject.Find("StageManager");
         fadeImage = GetComponent<Image>();
+
+        //SE
+        SE = GameObject.Find("AudioManage").GetComponent<AudioManager>();
     }
 
     // Update is called once per frame
@@ -29,30 +36,45 @@ public class StageSelect2 : MonoBehaviour
 
     public void onButtonStage2_1()
     {
+        buttonflag = GameObject.Find("2-1").GetComponent<Button>();
+        buttonflag.interactable = false;
+        SE.playselectSE();
         fadeImage.GetComponent<Fade>().GameStart();
         SM.GetComponent<StageManagerScript>().SetStageIndex(5);
     }
 
     public void onButtonStage2_2()
     {
+        buttonflag = GameObject.Find("2-2").GetComponent<Button>();
+        buttonflag.interactable = false;
+        SE.playselectSE();
         fadeImage.GetComponent<Fade>().GameStart();
         SM.GetComponent<StageManagerScript>().SetStageIndex(6);
     }
 
     public void onButtonStage2_3()
     {
+        buttonflag = GameObject.Find("2-3").GetComponent<Button>();
+        buttonflag.interactable = false;
+        SE.playselectSE();
         fadeImage.GetComponent<Fade>().GameStart();
         SM.GetComponent<StageManagerScript>().SetStageIndex(7);
     }
 
     public void onButtonStage2_4()
     {
+        buttonflag = GameObject.Find("2-4").GetComponent<Button>();
+        buttonflag.interactable = false;
+        SE.playselectSE();
         fadeImage.GetComponent<Fade>().GameStart();
         SM.GetComponent<StageManagerScript>().SetStageIndex(8);
     }
 
     public void onButtonStage2_5()
     {
+        buttonflag = GameObject.Find("2-5").GetComponent<Button>();
+        buttonflag.interactable = false;
+        SE.playselectSE();
         fadeImage.GetComponent<Fade>().GameStart();
         SM.GetComponent<StageManagerScript>().SetStageIndex(9);
     }
