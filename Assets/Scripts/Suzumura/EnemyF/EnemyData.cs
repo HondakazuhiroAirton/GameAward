5f0138d3db8292ec5e43d2d666dc40d194865c12
tooooooooo <to-kun1049@outlook.jp>
2023-05-23 21:04:50 +0900

    鈴村）更新

--- Assets/Scripts/Suzumura/EnemyF/EnemyData.cs
+++ Assets/Scripts/Suzumura/EnemyF/EnemyData.cs
@@ -22,15 +22,15 @@ public class EnemyData
     public string model;                // モデル読み込み(パス)
     public EnemyCategory category;      // カテゴリー
     public float Size;                  // 大きさ
-    public int State;                   // 状態
-    public int NextState1;               // 次の状態1
-    public int NextState2;               // 次の状態2
-    public int NextState3;               // 次の状態3
-    public int NextState4;               // 次の状態4
-    public int NextState5;               // 次の状態5
+    public int NextState1;              // 次の状態1
+    public int NextState2;              // 次の状態2
+    public int NextState3;              // 次の状態3
+    public int NextState4;              // 次の状態4
+    public int NextState5;              // 次の状態5
 
+    public int State;                   // 状態
     public Vector3 prevPosition;
-    public float distance_two;         //二点間の距離を入れる
+    public float distance_two;          //二点間の距離を入れる
     public float Duration;
     public float PresentLocation;
     public Vector3 Entry;
