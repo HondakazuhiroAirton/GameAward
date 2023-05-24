e0a5527bcff07b07aa403e41682d2ca680416fa6
mkngy <halkirby0423@gmail.com>
2023-05-25 00:41:53 +0900

    長屋）BGM調整中

--- Assets/Scripts/MiuraScript/GMC_BGMscript.cs
+++ Assets/Scripts/MiuraScript/GMC_BGMscript.cs
@@ -1,19 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using KanKikuchi.AudioManager;
 
 public class GMC_BGMscript : MonoBehaviour
 {
-   // public GameObject ResultPanel;
+    //シングルトン設定ここから
+    public static GMC_BGMscript instance;
 
-    public AudioClip GameBGM;
-    public AudioClip ResultBGM;
-    public AudioClip GameOverBGM;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+    //シングルトン設定ここまで
+
+
+    // public GameObject ResultPanel;
+
+    //BGM集
+    [SerializeField] private AudioSource BGM;
+    [SerializeField] private AudioClip TitleBgm;
+    [SerializeField] private AudioClip SelectBgm;
+    [SerializeField] private AudioClip GameBGM;
+    [SerializeField] private AudioClip ResultBGM;
+    [SerializeField] private AudioClip GameOverBGM;
+
+    private string beforeScene;//string型の変数beforeSceneを宣言 
 
     private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(this.gameObject);
+
+        BGM=this.GetComponent<AudioSource>();
+
+
+        beforeScene = "Title";
+        BGM.PlayOneShot(TitleBgm);
+
         // BGMの音を流す
         audioSource = this.GetComponent<AudioSource>();
         audioSource.clip = GameBGM;
@@ -26,23 +61,53 @@ public class GMC_BGMscript : MonoBehaviour
 
     }
 
-    // リザルト用BGM開始
-    public void ResultBGMStart()
+   // リザルト用BGM開始
+   public void ResultBGMStart()
+   {
+       // BGM止める
+       audioSource.Stop();
+       // BGMの音を流す
+       audioSource.clip = ResultBGM;
+       audioSource.Play();
+   }
+
+   // ゲームオーバー用用BGM開始
+   public void GameOverBGMStart()
+   {
+       // BGM止める
+       audioSource.Stop();
+       // BGMの音を流す
+       audioSource.clip = GameOverBGM;
+       audioSource.Play();
+   }
+
+    void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
     {
-        // BGM止める
-        audioSource.Stop();
-        // BGMの音を流す
-        audioSource.clip = ResultBGM;
-        audioSource.Play();
+        //シーンがどう変わったかで判定
+        //タイトルからステージセレクトへ
+        if (beforeScene == "Title" && nextScene.name == "nagaya")
+        {
+            BGM.Stop();
+            BGM.PlayOneShot(SelectBgm);
+        }
+
+        // ステージセレクトからゲーム画面へ(予定）
+        if (beforeScene == "nagaya" && nextScene.name == "Master_Final")
+        {
+            BGM.PlayOneShot(GameBGM);
+            BGM.Stop();
+        }
+
+        //ステージセレクトからタイトル画面へ
+        if (beforeScene == "nagaya" && nextScene.name == "Title")
+        {
+            //if()//タイトルへ戻るボタンを押したとき
+            BGM.Stop();
+            BGM.PlayOneShot(TitleBgm);
+        }
+        
+        //遷移後のシーン名を「１つ前のシーン名」として保持
+        beforeScene = nextScene.name;
     }
 
-    // ゲームオーバー用用BGM開始
-    public void GameOverBGMStart()
-    {
-        // BGM止める
-        audioSource.Stop();
-        // BGMの音を流す
-        audioSource.clip = GameOverBGM;
-        audioSource.Play();
-    }
 }
